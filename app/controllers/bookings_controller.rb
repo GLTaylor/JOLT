3d3ae7608f0ef8bc797a160617505db76ff6f788
camille raiser <camille.raiser@hotmail.fr>
2018-08-14 17:22:58 +0200

    comit before seed merge

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,16 @@
 class BookingsController < ApplicationController
+  before_action :set_nde
+
   def new
+    @booking = Booking.new
   end
 
   def create
+  end
+
+  private
+
+  def set_nde
+    @nde = Nde.find(params[:nde_id])
   end
 end
