066c832adc567cc6ed21acbd9330866c8af5639c
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-08-14 10:29:08 +0200

    card headers and a hardcoded image

--- app/controllers/ndes_controller.rb
+++ app/controllers/ndes_controller.rb
@@ -1,13 +1,14 @@
 class NdesController < ApplicationController
-  def index
-  end
+  # def index
+  # end
 
   def show
+    @nde = Nde.find(params[:id])
   end
 
-  def create
-  end
+  # def create
+  # end
 
-  def new
-  end
+  # def new
+  # end
 end
