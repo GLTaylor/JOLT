b2684f22d1d3c5e9f9866fe2acfc1e6b0074d4c5
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-08-15 11:09:47 +0200

    Adjustments to edit, photouploader added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+before_action: set user_params
   def show
     @user = User.find(params[:id])
   end
@@ -9,5 +9,11 @@ class UsersController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :photo)
   end
 end
