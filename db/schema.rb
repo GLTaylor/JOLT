9b6f89b3aa0ec7b9692e7ed2732dd9057dbc3372
Ginny Wan <zctywan@ucl.ac.uk>
2018-08-16 16:18:23 +0200

    autocomplete

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2018_08_16_101438) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_ndes_on_user_id"
   end
 
